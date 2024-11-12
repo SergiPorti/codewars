aa077a29c24ee447cc47bbda5d53528477ad5521
Sergi Portero <serporterolo01@gmail.com>
2024-11-12 22:25:23 +0100

    Refactor to good functionality in Problem PicPocPong

--- AceptaElReto/ProblemExecutor/ProblemExecutorImpl/PicPocPong.java
+++ AceptaElReto/ProblemExecutor/ProblemExecutorImpl/PicPocPong.java
@@ -19,20 +19,28 @@ public class PicPocPong extends ProblemExecutorAbstractClassName {
 
         Scanner sc = new Scanner(file);
 
-        String input = sc.next();
+        String input = "";
         boolean derecha = false;
         Map<String, Integer> map = new HashMap<>();
-        map.put("d", 0);
-        map.put("i", 0);
 
-        while (!input.equals("0")) {
-            input = sc.next();
-            derecha = "PIC".equals(input) ? !derecha : derecha;
+        while (sc.hasNextLine()) {
+            map.put("d", 0);
+            map.put("i", 0);
+            input = sc.nextLine();
+            if (input.equals("0"))
+                break;
+            String[] splittedInput = input.split(" ");
 
-            map.put(derecha ? "d" : "i", map.get(derecha ? "d" : "i") + 1);
+            for (int i = 1; i <= Integer.valueOf(splittedInput[0]); i++) {
+                derecha = "PIC".equals(splittedInput[i]) ? !derecha : derecha;
+
+                map.put(derecha ? "d" : "i",
+                        "PONG!".equals(splittedInput[i]) ? map.get(derecha ? "d" : "i") + 1
+                                : map.get(derecha ? "d" : "i"));
+            }
+            System.out.println(String.join(" ", String.valueOf(map.get("i")), String.valueOf(map.get("d"))));
         }
 
-        System.out.println(String.join(" ", String.valueOf(map.get("i")), String.valueOf(map.get("d"))));
         sc.close();
 
     }
